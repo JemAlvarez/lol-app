94af7be25de2573b36100fdb257bde2980a5ab96
Jem <jemalvarez22@gmail.com>
2019-04-21 15:46:17 -0400

    '/' -> endpoints

--- src/index.js
+++ src/index.js
@@ -10,6 +10,20 @@ app.use(express.json())
 app.use(championsRouter)
 app.use(summonerRouter)
 
+app.get('/', (req, res) => {
+    res.send({
+        summonerData: '/summoner/:region/:name',
+        findMatchById: '/match/:region/:summoner/:id',
+        findChampionById: '/champion/:id',
+        allChampions: '/champions',
+        randomSplashArt: '/splashart',
+        randomLane: '/random-lane',
+        randomChampion: '/random-champion',
+        freeChampRotation: '/rotation',
+        images: '/img/:folder/:name'
+    })
+})
+
 const port = process.env.PORT
 
 app.listen(port, () => {
