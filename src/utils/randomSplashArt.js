9ffdc71847ffc84db90c67a9536665c948b97957
Jem <jemalvarez22@gmail.com>
2019-04-23 01:22:54 -0400

    Fix #999999 for schedule task

--- src/utils/randomSplashArt.js
+++ src/utils/randomSplashArt.js
@@ -5,7 +5,7 @@ let splashArt = {
     splashArt: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Pyke_0.jpg'
 }
 
-cron.schedule('22 1 * * *', () => {
+cron.schedule('24 1 * * *', () => {
     getAllChampsArray().then(champions => {
         const item = champions[Math.floor(Math.random() * champions.length)]
         splashArt = { splashArt: item.splashImg }
